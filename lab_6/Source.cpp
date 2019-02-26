798e1f0a3a8caa51a0983e4974c86318dd50cbf2
breyaivey <48031722+breyaivey@users.noreply.github.com>
2019-02-26 14:58:40 -0600

    Add files via upload

---
+++ lab_6/Source.cpp
@@ -0,0 +1,84 @@
+// Breya Ivey
+// CMPS 1044 Dr.Johnson
+// 2/26/19
+// Lab 6 Switch Statements 
+// Description:  
+
+#include <iostream> 
+#include <fstream>
+using namespace std;
+int main()
+{
+	ofstream outfile;
+	outfile.open("output.txt");
+	outfile << "BreyaIvey\n\n";
+
+	int count = 1;
+	while (count <= 5)
+	{
+		int selection;
+
+
+		cout << "Which formula do you want to see?\n\n";
+		cout << "1. Area of a circle\n";
+		cout << "2. Area of a rectangle\n";
+		cout << "3. Volume of a cylinder\n";
+		cout << "4. None of them!\n";
+		cin >> selection;
+
+
+		switch (selection) 
+		{
+		case 1:
+			cout << "Pi times radius squared\n";
+			break;
+		case 2:
+			cout << "Length times width\n";
+			break;
+		case 3:
+			cout << "Pi times radius squared times height\n";
+			break;
+		case 4:
+			cout << "Well okay then...Goodbye!\n";
+			break;
+		default:
+			cout << "Not good with numbers, eh?\n";
+
+
+		}
+
+		// Insert cout, cin & switch statements here – output to file
+		count += 1;
+	}
+
+	system("pause");
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
